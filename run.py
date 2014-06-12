0e59cfd18b187e2f65fcf4b7c09dca1bc59db243
pjloury <pjloury@gmail.com>
2014-06-12 14:16:52 -0700

    fixed run.py file

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
 from app import app #from the app folder import the app object (from init) 
-app.run(debug = True)
+app.run('0.0.0.0', debug=True)
 #this is the path to the interpreter
 #./run.py: flask/bin/python: bad interpreter: No such file or directory
