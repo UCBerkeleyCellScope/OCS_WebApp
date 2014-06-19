1142765b68a71d6d846baa8dd6b5c26844bcd68e
pjloury <pjloury@gmail.com>
2014-06-19 12:36:29 -0700

    switching to postgres

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__)) #directory of config.py
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-'''
+
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 if os.environ.get('DATABASE_URL') is None:
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
   SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-'''
+
 
 print "Database URI "+SQLALCHEMY_DATABASE_URI
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')#join is like +
