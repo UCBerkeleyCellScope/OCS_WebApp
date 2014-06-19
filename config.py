a2db97335cc3ce3b3ee73f9359dbf1318e8d4456
pjloury <pjloury@gmail.com>
2014-06-19 12:18:16 -0700

    231

--- config.py
+++ config.py
@@ -3,8 +3,16 @@ basedir = os.path.abspath(os.path.dirname(__file__)) #directory of config.py
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-print "Database URI"+SQLALCHEMY_DATABASE_URI
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+'''
+if os.environ.get('DATABASE_URL') is None:
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+'''
+
+print "Database URI "+SQLALCHEMY_DATABASE_URI
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')#join is like +
 
 AWS_ACCESS_KEY_ID='AKIAIZJCDD43UTMHGLXQ'
