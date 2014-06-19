610aa4338134188d7430ed86eb467d0fb36f64d8
pjloury <pjloury@gmail.com>
2014-06-19 10:06:43 -0700

    postgres?

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__)) #directory of config.py
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')#join is like +
 
 AWS_ACCESS_KEY_ID='AKIAIZJCDD43UTMHGLXQ'
