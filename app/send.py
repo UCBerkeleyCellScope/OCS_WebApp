354a118fa1b6a69626a3047b4f7abc8452e2bf81
pjloury <pjloury@gmail.com>
2014-06-26 23:11:09 -0700

    asdf

--- app/send.py
+++ app/send.py
@@ -6,17 +6,19 @@ AUTH_TOKEN = "91fd2fc525d70eadd02aa82fc2b9083e"
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
+"""
 message = client.messages.create(body="Jenny please?! I love you <3",
     to="+14085291354",
     from_="+15104661333")
     #,media_url="http://www.example.com/hearts.png")
 print message.sid
+"""
 
 def examUploadConfirmation(exam_uuid,phoneNumber):
   client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
   print "INSIDE TWILIO METHOD"
   text= "Thank you for using the Ocular CellScope. Your exam has been uploaded and is being\
   reviewed by a clinician. You may view your exam at www.OCSWebApp.HerokuApp.com/select/glaucoma/"
-  +exam_uuid
+  +str(exam_uuid)
   message = client.messages.create(body=text,to=phoneNumber,from_="+15104661333")
   print message.sid
